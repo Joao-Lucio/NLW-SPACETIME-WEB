c9ca11e25f6b71211bf2ed60ecfd428d69dddf72
Joao-Lucio <joao.silvarrr@gmail.com>
2023-06-03 11:47:51 -0400

    alter-server

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 // informando qual é a rota do back end padrão
 export const api = axios.create({
-  baseURL: 'http://192.168.100.54:3333',
+  baseURL: 'http://localhost:3333',
 })
